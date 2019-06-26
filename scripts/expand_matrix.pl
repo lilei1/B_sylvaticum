9fe8c56abd13006589b19340755e10fe9a0f07fa
Li Lei <LLei@LLei-M59.dhcp.lbl.gov>
2019-06-26 15:08:05 -0700

    add perl scripts

---
+++ scripts/expand_matrix.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+##by Li Lei, 20190620,Walnut Creek;
+#this is to masked all of the number. Treat all of the non-zero as 1. This file is for making vann diagram
+#usage: 
+use strict;
+use warnings;
+use Data::Dumper;
+my $file = $ARGV[0];
+
+my %gidhash;
+
+
+open(SNPID,  "$file") or die "Could not open $file";
+my $header = <SNPID>;
+print "Bdistachyon\tBstacei\tBsylvaticum\tOsativaKitaake\tSbicolor\n";
+foreach my $row (<SNPID>){
+        chomp $row;
+        my @rtemp= split(/\t/,$row);
+        my $nb = $rtemp[5] - 1;
+         for (my $i=0; $i <= $nb; $i++ ){
+         	     print "$rtemp[0]\t$rtemp[1]\t$rtemp[2]\t$rtemp[3]\t$rtemp[4]\n";
+
+         }
+}
+close (SNPID);
---
+++ scripts/sum_count.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+##by Li Lei, 20190620,Walnut Creek;
+#this is to masked all of the number. Treat all of the non-zero as 1. This file is for making vann diagram
+#usage: 
+use strict;
+use warnings;
+use Data::Dumper;
+my $file = $ARGV[0];
+
+my %gidhash;
+
+
+open(SNPID,  "$file") or die "Could not open $file";
+#my $header = <SNPID>;
+#print "$header";
+foreach my $row (<SNPID>){
+        chomp $row;
+        my @rtemp= split(/\t/,$row);
+        my $key = $rtemp[0]."_".$rtemp[1]."_".$rtemp[2]."_".$rtemp[3]."_".$rtemp[4];
+        push @{$gidhash{$key}}, $rtemp[5];
+}
+close (SNPID);
+
+print "Bdistachyon\tBstacei\tBsylvaticum\tOsativaKitaake\tSbicolor\tcount\n";
+my $count=0;
+foreach my $key (keys %gidhash){
+ 			my @arrary = @{$gidhash{$key}};
+ 			foreach my $ele (@arrary){
+ 				$count = $count + $ele;
+ 			}
+ 			my @tt = split(/\_/,$key);
+ 			print "$tt[0]\t$tt[1]\t$tt[2]\t$tt[3]\t$tt[4]\t$count\n";
+ 			$count=0;
+}
