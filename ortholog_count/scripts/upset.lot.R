63ad7f92fea3b4a38f09f19906618ffb238acc83
Li Lei <LiLei@LiLei-M57.dhcp.lbl.gov>
2019-08-13 15:55:53 -0700

    replace upsetplot

--- ortholog_count/scripts/upset.lot.R
+++ ortholog_count/scripts/upset.lot.R
@@ -8,10 +8,27 @@ nrow(movies)
 upset(movies, sets = c("Action", "Adventure", "Comedy", "Drama", "Mystery", "Thriller", "Romance", "War"))
 
 
-data <- read.delim(file="/Users/LLei/Downloads/orthodbs/sorted_masked_gene_ortholog.matrix", header = T, sep = "\t")
+data <- read.delim(file="/Users/LiLei/Downloads/orthodbs_round2/sorted_masked_gene_ortholog.matrix", header = T, sep = "\t")
 head(data)
-pdf(file = "/Users/LLei/Downloads/orthodbs/upset_orth.pdf",width = 25,height = 15)
-upset(data,sets = c("Bdistachyon", "Bstacei", "Bsylvaticum", "OsativaKitaake", "Sbicolor"), order.by = c("degree"), mainbar.y.label="Orthologous clusters in each intersection",sets.x.label
-="Orthologous clusters in each species",text.scale=2.5,point.size=7)
+pdf(file = "/Users/LiLei/Downloads/orthodbs_round2/upset_orth.pdf",width = 42,height = 15)
+#upset(data,sets = c("Sbicolor","OsativaKitaake","PhalliiHAL","Bsylvaticum","Bstacei","Bdistachyon"), order.by = c("degree"), mainbar.y.label="Orthologous clusters in each intersection",sets.x.label
+#="Orthologous clusters in each species",text.scale=2.5,point.size=7)
+
+setsBarColors <- c('#4285F4','#4285F4', '#EA4335', '#FBBC05', '#FBBC05', '#FBBC05')
+
+upset(data,
+      nsets=6,
+      nintersects=1000,
+      sets = c("PhalliiHAL","Bsylvaticum","Sbicolor","Bstacei","Bdistachyon","OsativaKitaake"), 
+      keep.order = TRUE, 
+      mainbar.y.label="Orthologous clusters in each intersection",
+      sets.x.label="Orthologous clusters in each species",
+      text.scale=2.5,
+      point.size=7,
+      sets.bar.color=setsBarColors,
+      queries = list(list(query = intersects,params = list("Bstacei","Bdistachyon","OsativaKitaake"), color ='#FBBC05' , active = T),
+                     list(query = intersects,params = list("PhalliiHAL","Bsylvaticum"), color ='#4285F4' , active = T))
+)
 
 dev.off()
+
