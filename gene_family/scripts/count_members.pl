11cf7b3082ccd7b92054c4088033c53fd7c7c35f
Li Lei <LiLei@LiLei-M57.dhcp.lbl.gov>
2019-08-19 16:59:31 -0700

    add a count_member script

---
+++ gene_family/scripts/count_members.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+##by Li Lei, 20190819,Walnut Creek;
+#this is to count the members in each species for each cluster:
+use strict;
+use warnings;
+use Data::Dumper;
+my $file = $ARGV[0];
+
+my %gidhash;
+
+
+open(SNPID,  "$file") or die "Could not open $file";
+#my $header = <SNPID>;
+#print "$header";
+foreach my $row (<SNPID>){
+        chomp $row;
+        my @rtemp = split(/\t/,$row);
+        my @tmp = split(/\t/,$row);
+        
+        push @{$gidhash{$key}}, $rtemp[6];
+}
+close (SNPID);
+
+print "Sbicolor\tOsativaKitaake\tPhalliiHAL\tBsylvaticum\tBstacei\tBdistachyon\tcount\n";
+my $count=0;
+foreach my $key (keys %gidhash){
+ 			my @arrary = @{$gidhash{$key}};
+ 			foreach my $ele (@arrary){
+ 				$count = $count + $ele;
+ 			}
+ 			my @tt = split(/\_/,$key);
+ 			print "$tt[0]\t$tt[1]\t$tt[2]\t$tt[3]\t$tt[4]\t$tt[5]\t$count\n";
+ 			$count=0;
+}
